df2bf72930feec4dabc66f0162d6398e92438e71
R0zty <105313545+R0zty@users.noreply.github.com>
2022-05-18 13:37:53 -0300

    Ejercicio N° 2 finalizado.

---
+++ ejercicio_2.py
@@ -0,0 +1,85 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica y consola
+
+# Ahora los valores a operar deben ser ingresados por
+# consola con la función "input" como se ve a continuación
+print('Ingrese por consola el primer número entero a operar:')
+numero_1 = int(input())
+
+print('Ingrese por consola el segundo número entero a operar:')
+numero_2 = int(input())
+
+# Alumno: Imprima en pantalla los dos números enteros solicitados
+# print(....)
+
+# Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
+# numero_1, numero_2
+# Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
+# El resultado de sumar 4 y 2 es 6
+# NOTA: No coloque usted los nùmeros y resultados, use las variables
+
+# Suma
+
+# Resta
+
+# División
+
+# Multiplicación
+
+# Ahora los valores a operar deben ser ingresados por
+# consola con la función "input" como se ve a continuación
+print('ingrese por consola el primer numero entero a operar:')
+numero_1 = int(input())
+
+print('ingrese por consolo el segundo numero entero a operar')
+numero_2 = int(input())
+
+
+# Alumno: Imprima en pantalla los dos números enteros solicitados
+# print(....)
+print (numero_1 , numero_2)
+
+# Suma
+print('ingrese por consola primer numero entero a operar:')
+numero_1 = int(input())
+
+print('ingrese por consola el segundo numero enteroa operar:')
+numero_2 = int(input())
+
+print('el resultado de la suma ingresada es =' , numero_1 + numero_2)
+
+#Resta
+print('ingrese por consola el primer numero entero a operar:')
+numero_1 = int(input())
+
+print('ingrese por consola el segundo numero entero a operar:')
+numero_2 = int(input())
+
+print('el resultado de la resta ingresada es =' , numero_1 - numero_2)
+
+#Division
+print('ingrese por consola el primer numero entero a operar:')
+numero_1 = int(input())
+
+print('ingrese por consola el segundo numero entero a operar:')
+numero_2 = int(input())
+
+print('el resultado de la division ingresada es =' , numero_1 / numero_2)
+
+#Multiplicacion
+print('ingrese por consola el primer numero entero a operar:')
+numero_1 = int(input())
+
+print('ingrese por consola el segundo numero entero a operar:')
+numero_2 = int(input())
+
+print('el resultado de la multiplicacion ingresada es =' , numero_1 * numero_2)
+  
